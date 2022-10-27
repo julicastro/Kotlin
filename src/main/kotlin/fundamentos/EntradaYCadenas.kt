eec5d0e8b54690515cdf1b4e7a5d119d65d27d6e
julicastro <castroejulian@gmail.com>
2022-10-26 22:53:47 -0300

    fixing repo

--- src/main/kotlin/fundamentos/EntradaYCadenas.kt
+++ src/main/kotlin/fundamentos/EntradaYCadenas.kt
@@ -1,5 +1,26 @@
 package fundamentos
 
 fun main (){
-    
+
+    println("ingrese dato")
+    var dato = readLine()
+
+    if(dato == "" || dato == null){
+        println("error al cargar dato")
+    }else{
+        println("Dato cargado correctamente")
+        println("el dato es: \n $dato")
+        println("en mayuscula: " + dato.uppercase())
+
+        println("""
+        *** Datos ***
+        Nombre: $dato
+        Apelldo: $dato
+    """.trimIndent())
+    }
+
+
+
+
+
 }
